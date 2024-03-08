f90741160abbcdd58841ce907085a7ff1cd160fe
omjavheri-dreams-international <162565704+omjavheri-dreams-international@users.noreply.github.com>
2024-03-08 20:32:54 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Sidebar from './Components/Sidebar'
+import Login from './Components/Login'
 
 function App() {
 
@@ -12,6 +13,9 @@ function App() {
 
           <div >
             <Sidebar/>
+          </div>
+          <div>
+            <Login/>
           </div>
         
 
